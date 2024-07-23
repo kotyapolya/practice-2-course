76e1cb1da5e906a6daaf8bfbebd25dc600e10567
kotyapolya <catcat384384@gmail.com>
2024-07-23 20:46:19 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Music-player
+
+Цей проект є музичним плеєром, розробленим на VueJS.
+Користувачі можуть вибирати зі списку доступних тонів для відтворення. Плеєр підтримує додавання та керування списком треків. Будований регулятор гучності дозволяє користувачам налаштовувати рівень звуку на власний розсуд.
+Під час відтворення треку плеєр показує поточний час запису, дозволяючи користувачам легко орієнтуватися в тривалості мелодії, що прослуховується.
+
+![](image/main.jpg)
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/MusicList.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container" ref="childComponent">
+        <div class="musiclist">
+            <div>
+                <MusicItem
+                        v-for="(music, i) in playlist"
+                        v-bind:music="music"
+                        v-bind:index="i"
+                        v-bind:isplay_w="isplay"
+                        v-bind:pre_id="pre_id"
+                        v-bind:id="id"
+
+                        v-on:play-music="playmusic"
+                />
+            </div>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+    import MusicItem from '@/components/MusicItem.vue'
+
+    export default {
+        props: ['playlist', 'isplay', 'pre_id', 'id'],
+
+        name: "Music-item-list",
+
+        data() {
+            return {
+                icon_src: require('@/assets/play.png'),
+                current: {},
+                index: 0,
+                previndex: 0,
+                bccoloritem: "background: #424242",
+            }
+        },
+
+        components: {
+            MusicItem
+        },
+
+        created() {
+            this.current = this.playlist[this.index]
+        },
+
+        methods: {
+
+            playmusic(id) {
+                this.$emit('play-music', id)
+            },
+
+        }
+    }
+
+
+</script>
+
+<style scoped>
+
+    .musiclist {
+        font-size: 22px;
+        background-color: #424242;
+        height: 600px;
+    }
+
+
+</style>
